7fd41c5a4ecf2d4473a90857a6190d30ecb933f1
Antonio Cassano <antonio.cassano@nhs.net>
2022-12-19 14:47:50 +0000

    second commit

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -1,15 +1,28 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MainController {
 
   @GetMapping("/register")
   public String showForm(Model model) {
-    model.addAttribute(new User());
+    log.info("In showForm()");
+    User user = new User();
+    model.addAttribute(user);
     return "register_form";
+  }
+
+  @PostMapping("/register")
+  public String submitForm(@ModelAttribute("user") User user) {
+    log.info("In submitForm()");
+    log.info(String.valueOf(user));
+    return "register_success";
   }
 }
